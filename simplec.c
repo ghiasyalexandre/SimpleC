d92d3228e3dc5c9d63fc5875ee165904586c2d11
ghiasyalexandre <54648375+ghiasyalexandre@users.noreply.github.com>
2019-09-30 13:40:58 -0400

    Update simplec.c

--- simplec.c
+++ simplec.c
@@ -1,60 +1,153 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
-#define MAX 1024
+#define MAX 4096
 
-int isKeyword(char *str)
+int isSpace(char ch)
 {
-	if (!strcmp(str, "print"))
-		return 1;
+	if (ch == ' ' || ch == ';' || ch == '\n') return 1;
 	return 0;
 }
 
 int isDelimiter(char ch)
 {
 	if (ch == ' ' || ch == '+' || ch == '-' ||
-		ch == '*' || ch == '/' || ch == '%' ||
-		ch == ';') return 1;
+		ch == '*' || ch == '/' || ch == ';' ||
+		ch == '%') return 1;
 	return 0;
 }
 
-int isOperator(char ch)
+char *isOperator(char ch)
 {
-	if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%')
+	char *buffer = NULL;
+	if (ch == '+'){
+		buffer = malloc(sizeof(char) * 8);
+		strcpy(buffer, "add nsw");
+	} else if (ch == '-'){
+		buffer = malloc(sizeof(char) * 8);
+		strcpy(buffer, "sub nsw");
+	} else if (ch == '*'){
+		buffer = malloc(sizeof(char) * 8);
+		strcpy(buffer, "mul nsw");		
+	} else if (ch == '/'){
+		buffer = malloc(sizeof(char) * 5);
+		strcpy(buffer, "sdiv");		
+	} else if (ch == '%'){
+		buffer = malloc(sizeof(char) * 5);
+		strcpy(buffer, "srem");	
+	} else if (ch == ';'){
+		buffer = malloc(sizeof(char) * 5);
+		strcpy(buffer, "semi");
+	} else{
+		printf("INVALID operator!\n");
+		return NULL;
+	}
+	return buffer;
+}
+
+int isIdentifier(char *str)
+{
+	if (!strcmp(str, "print"))
 		return 1;
 	return 0;
 }
 
-void readFile(FILE *ifp)
+void printLLVM(int a, int b, int *count, char *operator)
 {
-	int i = 0;
-	char ch, *buffer;
-
-	while((ch = fgetc(ifp)) != EOF)
+	if (b == INT_MIN)
 	{
-		if (isDelimiter(ch))
-			continue;
-		
+		printf("\tcall void @print_intger(i32 %%t%d\n", a);
+		return;
 	}
+	printf("\t%%t%d = %s i32 %d, %d\n", *count, operator, a, b);
+	printf("\tcall void @print_intger(i32 %%t%d\n", *count);
+	(*count)++;
 }
 
-void printFormat(int i, int val1, int val2)
+void readFile(FILE *ifp)
 {
-	printf("\n");
+	char ch, *buffer = NULL, *operator = NULL;
+	int a = 0, b = 0, flag = 0, *count = 1, i = 0;
+
+	while ((ch = fgetc(ifp)) != EOF)
+	{
+		if (isSpace(ch))
+			continue;
+		else if (ch == 'p')
+		{
+			buffer = calloc(MAX, sizeof(char));
+			operator = calloc(10, sizeof(char));
+
+			for (i = 0; isDelimiter(ch) == 0; i++)
+			{
+				buffer[i] = ch;
+				ch = fgetc(ifp);
+			}
+
+			buffer[i] = '\0';
+			ch = fgetc(ifp)
+			while(isSpace(ch)) ch = fgetc(ifp);
+
+			if (isIdentifier(buffer))
+			{
+				if (isDigit(ch))
+				{
+					a = atoi(ch);
+					ch = fgetc(ifp);
+					while(isSpace(ch)){
+						ch = fgetc(ifp);
+					}
+					strcpy(operator, isOperator(ch));
+
+					if (operator == NULL)
+						break;
+					else if (!strcmp(operator, "semi"))
+						printLLVM(a,INT_MIN,NULL,NULL);
+					else
+					{
+						ch = fgetc(ifp);
+						while(isSpace(ch)){
+							ch = fgetc(ifp);
+						}
+
+						if (isDigit(ch))
+						{
+							b = atoi(ch);
+							printLLVM(a,b,count,operator);
+						}
+					}
+
+				}
+			}
+			else
+				printf("INCORRECT Identifier\n");
+
+			free(buffer);
+			free(operator);
+			a = b = i = flag = 0;
+			*count = 1;
+		}
+		else
+			printf("INCORRECT Identifier\n");
+	}
 }
 
 int main(int argc, char **argv)
 {
-	FILE *ifp, *ofp;
-	if (argv[1] == NULL) return 0;
+	int i = 0;
+	FILE *ifp;
+
+	if (argv == NULL) return 0;
 	if ((ifp = fopen(argv[1], "r")) == NULL) return 0;
-	if ((ofp = fopen("program.ll", "w")) == NULL) return 0;
 
-	printf("define i32 @main() #0 {\n");
+	printf("define void @print_intger(i32) #0 {\n");
+	printf("\n");
 
-	printf("\t ret #0\n");
+	printf("\tret i32 0\n");
 	printf("}\n");
 	readFile(ifp);
+
 	return 0;
 }
